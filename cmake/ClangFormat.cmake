8972b490793083a77bf094355024d3e0552f11cb
HNABLI <nablihatem3@gmail.com>
2025-02-27 23:52:46 +0100

    clang tidy and clang format configuration

---
+++ cmake/ClangFormat.cmake
@@ -0,0 +1,19 @@
+# Define a function to create a custom target for running clang-format
+
+function(add_clang_format_target TARGET_NAME SOURCE_DIR)
+    find_program(CLANG-FORMAT_PATH clang-format REQUIRED)
+
+    # Find all C++ source files in the specified directory
+    file(GLOB_RECURSE FORMAT_SOURCES
+        LIST_DIRECTORIES FALSE
+        "${SOURCE_DIR}/*.cpp"
+        "${SOURCE_DIR}/*.hpp"
+    )
+
+    # Create a custom target to run clang-format
+    add_custom_target(${TARGET_NAME}
+        COMMAND ${CLANG-FORMAT_PATH} -i ${FORMAT_SOURCES}
+        WORKING_DIRECTORY ${CMAKE_SOURCE_DIR} # Change this if needed
+        COMMENT "Ranning clang-format on ${SOURCE_DIR} sources"
+    )
+endfunction()
